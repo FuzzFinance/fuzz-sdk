3374affb9e5e370842c39bfa89ecdb016545fd0c
Michael Otis <mike.otis@gmail.com>
2021-07-31 14:57:48 -0500

    commit

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -4,9 +4,9 @@ import { SolidityType } from '../constants'
 import { validateSolidityTypeInstance } from '../utils'
 
 /**
- * A currency is any fungible financial instrument on Ethereum, including Ether and all ERC20 tokens.
+ * A currency is any fungible financial instrument on Harmony, including ONE and all HRC20 tokens.
  *
- * The only instance of the base class `Currency` is Ether.
+ * The only instance of the base class `Currency` is Harmony.
  */
 export class Currency {
   public readonly decimals: number
@@ -16,10 +16,10 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'BNB', 'BNB')
+  public static readonly HARMONY: Currency = new Currency(18, 'ONE', 'ONE')
 
   /**
-   * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
+   * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ONE`.
    * @param decimals decimals of the currency
    * @param symbol symbol of the currency
    * @param name of the currency
@@ -33,5 +33,5 @@ export class Currency {
   }
 }
 
-const ETHER = Currency.ETHER
-export { ETHER }
+const HARMONY = Currency.HARMONY
+export { HARMONY }
